5370113e1924e9b724c52c96fa0329d8624fa3db
Mika Rinquest <rinquestmika@gmail.com>
2022-10-10 17:26:59 +0200

    Backup

--- src/router/index.js
+++ src/router/index.js
@@ -2,3 +2,18 @@ import {createApp} from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
 
+import Home from "../views/Home.vue"
+
+const routes = [
+    {path:"/",
+name:"Home",
+component:Home
+}
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+})
+
+export default router
