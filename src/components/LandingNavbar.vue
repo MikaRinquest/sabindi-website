54cf15fa9561f26339dcdf3b3a5721754cf73360
Mika Rinquest <rinquestmika@gmail.com>
2022-10-10 16:39:58 +0200

    Backup

---
+++ src/components/LandingNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav>
+        <ul>
+            <router-link>
+                <li>Home</li>
+            </router-link>
+            <router-link>
+                <li>Services</li>
+            </router-link>
+            <router-link>
+                <li>About</li>
+            </router-link>
+            <router-link>
+                <li>Industries serviced</li>
+            </router-link>
+            <router-link>
+                <li>Contact</li>
+            </router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+li {
+    list-style-type: none;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <LandingNavbar />
+    <Landing />
+</template>
+
+<script>
+import Landing from "../components/Landing.vue"
+import LandingNavbar from "../components/LandingNavbar.vue"
+
+export default {
+    components: {
+        Landing,
+        LandingNavbar
+    }
+}
+</script>
+
+<style>
+
+</style>
